576a765661807f9251bc8509ad75efa41145af75
Minsang Yu <minsang_yu2@tmax.co.kr>
2021-11-29 16:03:16 +0900

    [bugfix] QR코드 생성 시 입력 텍스트가 없으면 에러가 뜨던 문제 해결

--- components/screens/GenerateScreen.js
+++ components/screens/GenerateScreen.js
@@ -4,17 +4,27 @@ import QRCodeGenerator from '../utils/QRCodeGenerator';
 
 const GenerateScreen = () => {
   // info for QR code
-  const [value, setValue] = useState('안녕하세요 :)');
+  const [inputValue, setInputValue] = useState('안녕하세요 :)');
+  const [qrValue, setQrValue] = useState(inputValue);
 
   const onChange = event => {
     const {text} = event.nativeEvent;
-    setValue(text);
+    if (text.length > 0) {
+      setQrValue(text);
+    } else {
+      setQrValue('ㅠㅅㅠ QR로 만들 텍스트가 없어요');
+    }
+    setInputValue(text);
   };
 
   return (
     <View>
-      <QRCodeGenerator value={value} />
-      <TextInput value={value} onChange={onChange} style={styles.textInput} />
+      <QRCodeGenerator value={qrValue} />
+      <TextInput
+        value={inputValue}
+        onChange={onChange}
+        style={styles.textInput}
+      />
     </View>
   );
 };
