0f2a768059cddba0703ce39d7d037a949aa2ece5
Minsang Yu <minsang_yu2@tmax.co.kr>
2021-12-01 15:48:06 +0900

    minor update

--- components/screens/ScanScreen.js
+++ components/screens/ScanScreen.js
@@ -1,14 +1,30 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import QRScanner from '../utils/scanners/react-native-camera';
+import {RNCamera} from 'react-native-camera';
+// import QRScanner from '../utils/scanners/react-native-camera';
 
 const ScanScreen = () => {
+  // camera ref
+  let camera;
+
   // detected QR code list
   const [qrList, setQrList] = useState([]);
 
+  // action after barcodes are detected
+  const QRDetected = ({barcodes}) => {
+    const newQrList = barcodes.map(barcode => barcode.data);
+    setQrList(newQrList);
+  };
+
   return (
     <View style={styles.container}>
-      <QRScanner setQrList={setQrList} style={styles.image} />
+      <RNCamera
+        ref={ref => (camera = ref)}
+        type={RNCamera.Constants.Type.back}
+        captureAudio={false}
+        onGoogleVisionBarcodesDetected={QRDetected}
+        style={styles.scanner}
+      />
       <Text style={styles.text}>QR Detected:</Text>
       {qrList.map(qr => (
         <Text style={styles.text}>{qr}</Text>
@@ -22,7 +38,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  image: {
+  scanner: {
     width: 200,
     height: 400,
   },
